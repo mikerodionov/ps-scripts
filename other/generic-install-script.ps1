e0719eaa45fe58dcb0874439886017a8df152fe2
Mikhail <rodionovmv@gmail.com>
2015-08-17 17:29:04 +0300

    Update generic-install-script.ps1

--- other/generic-install-script.ps1
+++ other/generic-install-script.ps1
@@ -1,5 +1,6 @@
 #Generic installation task
 #Detect OS bitness
+#Method 1 - [IntPtr]::Size - Gets the size of current PS instance. The value of this property is 4 for 32-bit process, and 8 for 64-bit process
 [IntPtr]::Size
 #Stop service if required
 #Backup database
