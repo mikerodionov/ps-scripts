d98a1565d7ad7ef37d203c322bfaf5793b7cb60d
Mikhail <rodionovmv@gmail.com>
2017-02-04 22:17:35 +0300

    Update create-k2-site.ps1

--- create-k2-site.ps1
+++ create-k2-site.ps1
@@ -1,1 +1,14 @@
+#Define variables as needed for your environment:
+$K2FarmNameFQDN='K2.conundrum.com'	
+$SiteName = "K2"
 
+#Create self-signed certificate
+New-SelfSignedCertificate -DnsName $K2FarmNameFQDN -CertStoreLocation cert:Localmachine\My
+
+#Create web site
+$SiteFolder = Join-Path -Path 'C:\inetpub\wwwroot' -ChildPath $SiteName
+New-WebSite -Name $SiteName -PhysicalPath $SiteFolder -Force
+$IISSite = "IIS:\Sites\$SiteName"
+Set-ItemProperty $IISSite -name  Bindings -value @{protocol="https";bindingInformation="*:443:$K2Farm"}
+#Assign certificate to site bindinf
+Get-ChildItem cert:\LocalMachine\My | where { $_.Subject -match $K2FarmNameFQDN } | select -First 1 | New-Item IIS:\SslBindings\0.0.0.0!443
