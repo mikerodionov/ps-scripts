f305c18a3977abfa6bd83e0992a133ee9a0e4c72
Mikhail <rodionovmv@gmail.com>
2017-03-09 09:03:12 +0300

    Create verify-db.collation.sql

---
+++ SQL/verify-db.collation.sql
@@ -0,0 +1,30 @@
+--This script shows default DB collation + all tables and columns with collation other than default one
+DECLARE @DatabaseCollation VARCHAR(100)
+
+SELECT 
+    @DatabaseCollation = collation_name 
+FROM 
+    sys.databases
+WHERE 
+    database_id = DB_ID()
+
+SELECT 
+    @DatabaseCollation 'Default database collation'
+
+SELECT 
+    t.Name 'Table Name',
+    c.name 'Col Name',
+    ty.name 'Type Name',
+    c.max_length,
+    c.collation_name,
+    c.is_nullable
+FROM 
+    sys.columns c 
+INNER JOIN 
+    sys.tables t ON c.object_id = t.object_id
+INNER JOIN 
+    sys.types ty ON c.system_type_id = ty.system_type_id    
+WHERE 
+    t.is_ms_shipped = 0
+    AND 
+    c.collation_name <> @DatabaseCollation
