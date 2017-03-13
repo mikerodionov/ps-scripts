e956f2c48e88e4d146fecfedd59d1a5cb71d773e
Mikhail <rodionovmv@gmail.com>
2017-03-13 23:16:27 +0300

    Update remove-unused-features-payload.ps1

--- one-liners/remove-unused-features-payload.ps1
+++ one-liners/remove-unused-features-payload.ps1
@@ -1,2 +1,3 @@
-#Remove all payload for not installed/enabled Windows features
+# Remove all payload for not installed/enabled Windows features
+# This feature is known as "Features on demand" and introduced in Server 2012/Windows 8
 Get-WindowsFeature | Where-Object {$._Installed -eq $False} | Uninstall-WindowsFeature -Remove
