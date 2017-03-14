6b722edc1215ca46cfe4a72efd7599dd21120eb9
Mikhail <rodionovmv@gmail.com>
2017-03-14 22:46:11 +0300

    Update netsh-firewall-management.ps1

--- 70-698/netsh-firewall-management.ps1
+++ 70-698/netsh-firewall-management.ps1
@@ -1,2 +1,6 @@
-# Allow program via WF using netsh firewall (deprecated) - replacement netsh advfirewall firewall
-netsh firewall add allowedprogram c:\windows\system32\tracert.exe "Trace Route" ENABLE
+# Allow program via WF using netsh firewall (deprecated) - replacement netsh advfirewall firewall (WFAS)
+netsh firewall add allowedprogram c:\windows\system32\tracert.exe "Trace Route" enable
+# Applying the same rule to Private Networks:
+netsh firewall add allowedprogram c:\windows\system32\tracert.exe "Trace Route" enable custom 0.0.0.0-255.255.255.255 standard
+# POSH alternative:
+New-NetFirewallRule -DisplayName "Trace Route" -Direction Inbound -Program "c:\windows\system32\tracert.exe" -Action Allow
