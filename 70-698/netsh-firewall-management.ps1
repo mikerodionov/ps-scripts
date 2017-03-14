240e142d354be992cd43b7695df21f0c035be9b6
Mikhail <rodionovmv@gmail.com>
2017-03-14 22:41:12 +0300

    Create netsh-firewall-management.ps1

---
+++ 70-698/netsh-firewall-management.ps1
@@ -0,0 +1,2 @@
+# Allow program via WF using netsh firewall (deprecated) - replacement netsh advfirewall firewall
+netsh firewall add allowedprogram c:\windows\system32\tracert.exe "Trace Route" ENABLE
