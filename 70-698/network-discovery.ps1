e3809e988ffb54805c8acd335478c3433188eaac
Mikhail <rodionovmv@gmail.com>
2017-03-15 08:55:06 +0300

    Update network-discovery.ps1

--- 70-698/network-discovery.ps1
+++ 70-698/network-discovery.ps1
@@ -1,2 +1,8 @@
+# Client Network discovery functionality depends on the following services:
+# DNS Client
+# Function Discovery Resource Publication
+# SSDP Discovery (Simple Service Discovery Protocol)
+# UPnP Device Host
+
 # Disable Network Discovery
 netsh advfirewall firewall set rule group="Network Discovery" new enable=No
