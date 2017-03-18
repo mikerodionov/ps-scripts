474507e165022555d39b19d4b82209f04aecdf03
Mikhail <rodionovmv@gmail.com>
2017-03-18 11:27:04 +0300

    Update disk-management.ps1

--- 70-698/disk-management.ps1
+++ 70-698/disk-management.ps1
@@ -8,3 +8,6 @@ Initialize-Disk -Number 2 -PartitionStyle GPT
 New-Partition -DiskNumber 2 -UseMaximumSize -AssignDriveLetter
 # Format volume:
 Format-Voldume -DriveLetter X -FileSystem NTFS
+
+# Check disk in online, but bad sectors scan/repair is still offline operation which requires reboot:
+chkdsk /f /r
