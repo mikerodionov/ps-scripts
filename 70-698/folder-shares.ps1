4893511763e57a142f737cf0d907fc2dbc3a728e
Mikhail <rodionovmv@gmail.com>
2017-03-18 17:08:04 +0300

    Update folder-shares.ps1

--- 70-698/folder-shares.ps1
+++ 70-698/folder-shares.ps1
@@ -3,3 +3,6 @@ net share MyShareName=c:\Share /GRANT:User,READ
 
 # PS alternative (by default shared with Read right for Everyone):
 New-SMBShare -Path c:\Share -Name Share
+
+#List shares (shared folder special icon is gone in Win 10):
+Get-SMBShare
