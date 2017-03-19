feec7f32674ded9a2ba69f98cd4cc165037106bb
Mikhail <rodionovmv@gmail.com>
2017-03-19 23:44:21 +0300

    Update create-site-collection.ps1

--- SharePoint/create-site-collection.ps1
+++ SharePoint/create-site-collection.ps1
@@ -1,3 +1,11 @@
+#Load SharePoint PS Module (if not already loaded)
+$ver = $host | select version
+if ($ver.Version.Major -gt 1) {$host.Runspace.ThreadOptions = "ReuseThread"} 
+if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+}
+
 # Use this cmdlet to get a list of available templates:
 Get-SPWebTemplate
 # Create SP site using specified template:
