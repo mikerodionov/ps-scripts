1499064728466619642ecdcf69d7a2858fee4ad3
Mikhail <rodionovmv@gmail.com>
2017-03-19 23:14:07 +0300

    Update create-site-collection.ps1

--- SharePoint/create-site-collection.ps1
+++ SharePoint/create-site-collection.ps1
@@ -1,3 +1,5 @@
+# Use this cmdlet to get a list of available templates:
 Get-SPWebTemplate
+# Create SP site using specified template:
 $Template = Get-SPWebTemplate "STS#0"
 New-SPSite -Url "https://cndrm-srv03.apps.conundrum.com" -OwnerAlias "conundrum\administrator" -Template $template
