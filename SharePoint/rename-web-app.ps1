48456267c34d826326ca9ae269c249699f0b5377
Mikhail <rodionovmv@gmail.com>
2017-03-19 12:58:12 +0300

    Create rename-web-app.ps1

---
+++ SharePoint/rename-web-app.ps1
@@ -0,0 +1,16 @@
+#Load SharePoint PS Module (if not already loaded)
+$ver = $host | select version
+if ($ver.Version.Major -gt 1) {$host.Runspace.ThreadOptions = "ReuseThread"} 
+if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+}
+
+# Rename web app, adjust variables below as required:
+$old_wa_name="SharePoint - 80"
+$new_wa_name="Portal"
+$WebApp = SPWebApplication | where {$_.Name -match $old_wa_name}
+$WebApp.Name =$new_wa_name
+$WebApp.Update()
+# This bit is just to verify it updated correctly
+Get-SPWebApplication | where {$_.Name -match $new_wa_name}
