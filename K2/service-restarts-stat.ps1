3ae65c92e04befef3b13849b16dc235aa08d2336
Mikhail <rodionovmv@gmail.com>
2017-03-27 15:38:35 +0300

    Update service-restarts-stat.ps1

--- K2/service-restarts-stat.ps1
+++ K2/service-restarts-stat.ps1
@@ -1,1 +1,3 @@
-Get-EventLog -LogName system -Source "service control manager" |?{$_.message -match "blackpearl"} | select timegenerated, message -First 30
+# This script will grab 30 latest service restart event I write output into C:\%servername%.xls
+# Run on K2 server which hosts K2 service
+Get-EventLog -LogName system -Source "service control manager" |?{$_.message -match "blackpearl"} | select timegenerated, message -First 30 | Export-Csv "C:\Servie_Restarts_$env:computername.xls"
