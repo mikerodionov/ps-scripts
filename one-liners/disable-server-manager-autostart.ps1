fa25efb9a49f56f58b610b256f17221f09cf3963
Mikhail <rodionovmv@gmail.com>
2017-03-30 16:34:48 +0300

    Update disable-server-manager-autostart.ps1

--- one-liners/disable-server-manager-autostart.ps1
+++ one-liners/disable-server-manager-autostart.ps1
@@ -1,1 +1,1 @@
-New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLog
+New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" –Force
