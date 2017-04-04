2dca1b82a68b6421d2eb50a82af4f35fb913a5f1
Mikhail <rodionovmv@gmail.com>
2017-04-04 18:48:32 +0300

    Update create-web-app.ps1

--- SharePoint/create-web-app.ps1
+++ SharePoint/create-web-app.ps1
@@ -1,4 +1,4 @@
-#Load SharePoint PS Module (if not already loaded)
+# Load SharePoint PS Module (if not already loaded)
 $ver = $host | select version
 if ($ver.Version.Major -gt 1) {$host.Runspace.ThreadOptions = "ReuseThread"} 
 if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
@@ -6,6 +6,7 @@ if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinu
     Add-PSSnapin "Microsoft.SharePoint.PowerShell"
 }
 
+# Create new web application
 $ap = New-SPAuthenticationProvider
 New-SPWebApplication -Name "Conundrum Apps" -Port 443 -Url "https://cndrm-srv03.apps.conundrum.com" `
 -ApplicationPool "Conundrum Apps" -ApplicationPoolAccount (Get-SPManagedAccount "conundrum\svc-sp") `
