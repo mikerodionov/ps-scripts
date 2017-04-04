17674064ad365f96fd646b10ee2b10f2b0fcb895
Mikhail <rodionovmv@gmail.com>
2017-04-04 21:18:53 +0300

    Create upgrade-and-migration.ps1

---
+++ 70-698/upgrade-and-migration.ps1
@@ -0,0 +1,3 @@
+# changepk command CANNOT be used to upgrade from one VERSION of Windows to another VERSION of Windows. 
+# You CAN use the changepk.exe command-line tool to upgrade to ANOTHER EDITION of the SAME VERSION of Windows. 
+changepk.exe /ProductKey <enter your new product key here>
