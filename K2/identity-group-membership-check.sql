77b830694a2c5d380248f47a9b50956cc43e3eff
Mikhail <rodionovmv@gmail.com>
2017-04-25 10:22:08 +0300

    Create identity-group-membership-check.sql

---
+++ K2/identity-group-membership-check.sql
@@ -0,0 +1,7 @@
+# This scripts shows specific group membership according to current state of K2 identity cache
+SELECT i.ID, i.FQN, i.ContainersResolved, i.MembersResolved from [Identity].[Identity] as i
+JOIN [Identity].IdentityMember as m on i.id = m.MemberID 
+WHERE m.ContainerID = 1026 -- Specify group ID from [identity].[identity]
+UNION 
+SELECT [ID], [FQN], [ContainersResolved], [MembersResolved] from [identity].[identity]
+WHERE [ID] = 1026 -- Specify group ID from [identity].[identity]
