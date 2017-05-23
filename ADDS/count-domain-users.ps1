5a18210eb56ee4d0857fd5096a51a1230392fe75
Mikhail <rodionovmv@gmail.com>
2017-05-23 21:09:01 +0300

    Update count-domain-users.ps1

--- ADDS/count-domain-users.ps1
+++ ADDS/count-domain-users.ps1
@@ -1,2 +1,3 @@
 # Count number of users in specific domain
+# Tested on Server 2008 R2, Server 2012 R2
 (Get-ADUser -Filter 'objectClass -eq "user" -and objectCategory -eq "person"' -SearchBase "DC=denallix,DC=com" -Properties SamAccountName | Measure-Object).Count
