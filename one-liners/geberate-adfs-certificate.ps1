51c9fa132920aa8ad83ce668e5bb6f8d4d583e14
Mikhail <rodionovmv@gmail.com>
2017-06-23 22:11:50 +0300

    Create geberate-adfs-certificate.ps1

---
+++ one-liners/geberate-adfs-certificate.ps1
@@ -0,0 +1,1 @@
+New-SelfSignedCertificateEx  -Subject 'CN=adfs.conundrum.com' -ProviderName "Microsoft Enhanced RSA and AES Cryptographic Provider"  -KeyLength 2048 -FriendlyName 'OAFED SelfSigned' -SignatureAlgorithm sha256 -EKU "Server Authentication", "Client authentication" -KeyUsage "KeyEncipherment, DigitalSignature" -Exportable -StoreLocation "LocalMachine"
