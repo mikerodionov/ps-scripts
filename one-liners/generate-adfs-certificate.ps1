867d39d01c37f1a841e0abc52b6167a8141bda83
Mikhail <rodionovmv@gmail.com>
2017-06-23 22:20:32 +0300

    Update generate-adfs-certificate.ps1

--- one-liners/generate-adfs-certificate.ps1
+++ one-liners/generate-adfs-certificate.ps1
@@ -1,2 +1,5 @@
 #This requires PSPKI - https://pspki.codeplex.com
-New-SelfSignedCertificateEx  -Subject 'CN=adfs.conundrum.com' -ProviderName "Microsoft Enhanced RSA and AES Cryptographic Provider"  -KeyLength 2048 -FriendlyName 'OAFED SelfSigned' -SignatureAlgorithm sha256 -EKU "Server Authentication", "Client authentication" -KeyUsage "KeyEncipherment, DigitalSignature" -Exportable -StoreLocation "LocalMachine"
+Import-Module PSPKI
+New-SelfSignedCertificateEx  -Subject 'CN=adfs.conundrum.com' -ProviderName "Microsoft Enhanced RSA and AES Cryptographic Provider" 
+`-KeyLength 2048 -FriendlyName 'OAFED SelfSigned' -SignatureAlgorithm sha256 -EKU "Server Authentication", "Client authentication" 
+`-KeyUsage "KeyEncipherment, DigitalSignature" -Exportable -StoreLocation "LocalMachine"
