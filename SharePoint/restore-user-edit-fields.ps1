e8c8d89281b0bdc034ecd197ba94401e33a829dd
Mikhail <rodionovmv@gmail.com>
2017-06-26 15:49:10 +0300

    Update restore-user-edit-fields.ps1

--- SharePoint/restore-user-edit-fields.ps1
+++ SharePoint/restore-user-edit-fields.ps1
@@ -1,3 +1,12 @@
+# Load SharePoint PS Module (if not already loaded)
+# You only need this section if you want to run this script from PS ISE / outside of SP Management Shell
+$ver = $host | select version
+if ($ver.Version.Major -gt 1) {$host.Runspace.ThreadOptions = "ReuseThread"} 
+if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+}
+
 # Tested with SharePoint 2010
 # Restores editable user fields after UPA provisioning
 $web = Get-SPWeb http://intranet.contoso.com/
