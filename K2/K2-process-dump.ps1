f6c5fcd8fdefa2f5d8f2426bfb96b35e36571fd2
Mikhail <rodionovmv@gmail.com>
2017-07-06 17:01:12 +0300

    Update K2-process-dump.ps1

--- K2/K2-process-dump.ps1
+++ K2/K2-process-dump.ps1
@@ -1,5 +1,16 @@
+# Obtain K2 service PID
 $procid=get-process "K2HostServer" |select -expand id
+# Create target folder if not exist:
+$DumpPath="C:\dumps"
+If (!(Test-Path $DumpPath))
+    {
+    Write-Host 'Creating folder'
+    (New-Item $DumpPath -ItemType Directory | out-null)
+    } 
+    Else {
+    Write-Host 'Folder already exist'
+    }
 # -ma - Write a full dump file which includes All the Image, Mapped and Private memory
 # -ma - Just takes a full dump immidiately from running process
 # -e -ma  - takes full dump file on unhandled exception aka crash dump
-C:\procdump\procdump.exe -accepteula -ma $procid C:\procdump\k2hostserver.dmp 
+C:\procdump\procdump.exe -accepteula -ma $procid C:\procdump\k2hostserver.dmp
