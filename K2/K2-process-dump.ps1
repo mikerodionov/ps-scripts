36fd0845970a35542961b86a30cb75f317e6d391
Mikhail <rodionovmv@gmail.com>
2017-07-06 17:04:47 +0300

    Update K2-process-dump.ps1

--- K2/K2-process-dump.ps1
+++ K2/K2-process-dump.ps1
@@ -13,4 +13,4 @@ If (!(Test-Path $DumpPath))
 # -ma - Write a full dump file which includes All the Image, Mapped and Private memory
 # -ma - Just takes a full dump immidiately from running process
 # -e -ma  - takes full dump file on unhandled exception aka crash dump
-C:\procdump\procdump.exe -accepteula -ma $procid C:\procdump\k2hostserver.dmp
+C:\procdump\procdump.exe -accepteula -ma $procid $DumpPath\k2hostserver.dmp
