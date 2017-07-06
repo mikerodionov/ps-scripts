926fe018a34e93831fb87694b0f0fad0c9010789
Mikhail <rodionovmv@gmail.com>
2017-07-06 12:51:25 +0300

    Update K2-process-dump.ps1

--- K2/K2-process-dump.ps1
+++ K2/K2-process-dump.ps1
@@ -1,5 +1,5 @@
 $procid=get-process "K2HostServer" |select -expand id
 # -ma - Write a full dump file which includes All the Image, Mapped and Private memory
 # -ma - Just takes a full dump immidiately from running process
-# -e -ma  - tales full dump file on unhandled exception aka crash dump
+# -e -ma  - takes full dump file on unhandled exception aka crash dump
 C:\procdump\procdump.exe -accepteula -ma $procid C:\procdump\k2hostserver.dmp 
