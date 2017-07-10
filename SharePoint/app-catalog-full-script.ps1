41bcebe2388669d842ed6ab22d6ed2dc027b91de
Mikhail <rodionovmv@gmail.com>
2017-07-10 13:18:37 +0300

    Create app-catalog-full-script.ps1

---
+++ SharePoint/app-catalog-full-script.ps1
@@ -0,0 +1,10 @@
+# Variables to configure
+$webapp='https://portal.infanova.com'
+
+# Check/create app domain and wildcard entry
+
+# Subscription Settings Service (establishes subdomains for apps and generates app IDs)
+
+# App Management Service (handles app permissions and licensing information)
+
+# Configure app URLs
