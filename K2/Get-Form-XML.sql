068e1047db4591b8d6062bb7126b2573fa1a5801
Mikhail <rodionovmv@gmail.com>
2017-07-11 11:45:16 +0300

    Create Get-Form-XML.sql

---
+++ K2/Get-Form-XML.sql
@@ -0,0 +1,10 @@
+--Query to pull FORM XML from K2 DB
+--Adjust form name below
+DECLARE @FormName nvarchar(200) = 'MyTestForm';
+DECLARE @FormID uniqueidentifier;
+SELECT @FormID = (SELECT [ID] FROM [K2].[Form].[Form]
+  WHERE [Name] = @FormName);
+
+SELECT *
+FROM [Form].[AuditLog]
+WHERE Data = CAST(@FormID AS varchar(64)) ORDER BY ID DESC
