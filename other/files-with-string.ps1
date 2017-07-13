8a12be0f441f12a3eb7089e415c4bfece39eb027
Mikhail <rodionovmv@gmail.com>
2017-07-13 16:07:29 +0300

    Update files-with-string.ps1

--- other/files-with-string.ps1
+++ other/files-with-string.ps1
@@ -1,3 +1,4 @@
+# Search for specified string through all files in specified directory, filtering files to search in
 $dir='C:\Program Files (x86)\K2 blackpearl\Setup\Log'
 $string='Logged Error'
 Get-ChildItem  $dir -recurse -Include *.log -Exclude *.exe | Select-String -pattern $string | group path | select name
