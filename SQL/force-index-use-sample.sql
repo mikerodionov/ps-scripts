18a48ce308703e213827829e45804a3c8dd5482b
Mikhail <rodionovmv@gmail.com>
2017-07-19 18:06:52 +0300

    Update force-index-use-sample.sql

--- SQL/force-index-use-sample.sql
+++ SQL/force-index-use-sample.sql
@@ -1,6 +1,8 @@
 -- When SQL Server query optimizer opt out not to use index (because of poor index selectivity)
 -- but you still want to use it
 -- You may use database hint is your query fo rorce use of specific index
+-- In realworld production scenarios you should trust query optimizer and avoid using hints,
+-- unless sepcifically instructed by MSFT support
 
 SELECT * 
   FROM TableName WITH (Index(IX_TableName_ColumnName)
