9abdb80c2ba80a7bf8b0df29735ef09439c4f2e3
Mikhail <rodionovmv@gmail.com>
2017-07-19 22:08:38 +0300

    Update index-drop-create.sql

--- SQL/index-drop-create.sql
+++ SQL/index-drop-create.sql
@@ -1,8 +1,15 @@
-# Create index
-# Note that columns order within index matters - seek is possible on first column only
+-- Create index
+-- Note that columns order within index matters - seek is possible on first column only
 CREATE INDEX IX_Tablename_Column1Colum2ColumnNNames
   ON TalbeName (Column1, Column2, Column N);
+  
+-- Create Covering Index - which includes data not participating in index sort
+-- these columns added with INCLUDE clause allow you to return data from the index itself
+-- without performing key look up operation to find the data row in the table
+CREATE INDEX IX_Tablename_ColumnName
+  ON TalbeName (ColumnName)
+  INCLUDE (Column2Name, Column3Name);
 
-# Drop index
+-- Drop index
 DROP INDEX IX_Tablename_Column1Colum2ColumnNNames
   ON TableName;
