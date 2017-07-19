3e666be60c3f558d8864a859d27573c3c713aa42
Mikhail <rodionovmv@gmail.com>
2017-07-19 22:11:14 +0300

    Update index-drop-create.sql

--- SQL/index-drop-create.sql
+++ SQL/index-drop-create.sql
@@ -6,6 +6,8 @@ CREATE INDEX IX_Tablename_Column1Colum2ColumnNNames
 -- Create Covering Index - which includes data not participating in index sort
 -- these columns added with INCLUDE clause allow you to return data from the index itself
 -- without performing key look up operation to find the data row in the table
+-- note that as only subset of columns included in this index it covers only this subset
+-- and can't be used in SELECT * statements which broader than index coverage
 CREATE INDEX IX_Tablename_ColumnName
   ON TalbeName (ColumnName)
   INCLUDE (Column2Name, Column3Name);
