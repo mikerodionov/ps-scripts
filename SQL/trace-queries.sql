9bfd8a4f7d366dc58e07f20705160010fb97a5a0
Mikhail <rodionovmv@gmail.com>
2017-07-20 17:40:34 +0300

    Create trace-queries.sql

---
+++ SQL/trace-queries.sql
@@ -0,0 +1,17 @@
+-- List server side traces
+SELECT * FROM sys.fn_trace_getinfo(0);
+
+-- List server side traces (easier to read form):
+SELECT
+  traceid AS TraceId
+  max(case when property = 2 then value end) AS TraceFile,
+  max(case when property = 3 then value end) AS MaxSize,
+  max(case when property = 4 then value end) AS StopTime,
+  max(case when property = 5 then
+    case value
+      when 1 then 'Running'
+      when 0 then 'Stopped'
+    end
+  end) AS CurrentStatus
+FROM sys.fn_trace_getinfo(0)
+GROUP BY traceid;
