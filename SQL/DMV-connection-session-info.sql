95097d6f1588e4d53a47e71dfcadf78f6ee7cb02
Mikhail <rodionovmv@gmail.com>
2017-07-20 11:05:09 +0300

    Create DMV-connection-session-info.sql

---
+++ SQL/DMV-connection-session-info.sql
@@ -0,0 +1,34 @@
+-- More details: http://buildingbettersoftware.blogspot.ru/2016/04/using-dmvs-to-find-sql-server.html
+-- Requires SQL Server 2012 or newer
+SELECT
+    database_id,   
+    session_id,
+    status,
+    login_time,
+    cpu_time,
+    memory_usage,
+    reads,
+    writes,
+    logical_reads,
+    host_name,
+    program_name,
+    host_process_id,
+    client_interface_name,
+    login_name as database_login_name,
+    last_request_start_time
+FROM sys.dm_exec_sessions
+WHERE is_user_process = 1
+ORDER BY cpu_time DESC;
+
+-- Query below groups information by login and cleint process
+SELECT
+    login_name,
+    host_name,
+    host_process_id,
+    COUNT(1) As LoginCount
+FROM sys.dm_exec_sessions
+WHERE is_user_process = 1
+GROUP BY
+    login_name,
+    host_name,
+    host_process_id;
