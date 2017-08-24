39965ffda2a11a583ddbd56a3182c1126199df74
Mikhail <rodionovmv@gmail.com>
2017-08-24 18:07:37 +0300

    Update create-SQL-alias.ps1

--- SQL/create-SQL-alias.ps1
+++ SQL/create-SQL-alias.ps1
@@ -9,20 +9,26 @@ $32bit = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\"
 ## chect if the ConnectTo SubFolder 
 ## exist in the registry if not it will create it.
 If((Test-Path -Path ($64bit + "MSSQLServer\Client\ConnectTo"))-ne $true)
-    {$writable = $true
-        $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("ConnectTo")
+    {
+    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("ConnectTo")
     }
-    If((Test-Path -Path ($32bit + "MSSQLServer\Client\ConnectTo"))-ne $true)
-    {        $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("ConnectTo")
- 
+If((Test-Path -Path ($32bit + "MSSQLServer\Client\ConnectTo"))-ne $true)
+    {
+    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("ConnectTo")
+    }
+
+If((Test-Path -Path ($64bit + "MSSQLServer\Client\SuperSocketNetLib"))-ne $true)
+    {
+    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("SuperSocketNetLib")
+    }
+If((Test-Path -Path ($32bit + "MSSQLServer\Client\SuperSocketNetLib"))-ne $true)
+    {
+    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("SuperSocketNetLib")
     }
  
 ## Create the TCPAlias
 $TCPAliasName="DBMSSOCN,"+$ServerName
- 
-
 New-ItemProperty -Path ($64bit + "MSSQLServer\Client\ConnectTo") -Name $AliasName -PropertyType string -Value $TCPAliasName
-}
 New-ItemProperty -Path ($32bit + "MSSQLServer\Client\ConnectTo") -Name $AliasName -PropertyType string -Value $TCPAliasName 
 
 # Enable TCP/IP
