9f17463adec241eede40420ee8e9217adac02d23
Mikhail <rodionovmv@gmail.com>
2017-09-27 11:43:24 +0300

    Update capacity-planning-check-instance-per-month.sql

--- K2/capacity-planning-check-instance-per-month.sql
+++ K2/capacity-planning-check-instance-per-month.sql
@@ -1,4 +1,5 @@
 --Number of instances used over a monthly basis
 --More details: https://johnnyk2com.wordpress.com/2015/08/11/how-do-you-plan-for-k2-database-sizing-for-performance/
+USE K2
 SELECT YEAR(StartDate) AS [Year], MONTH(StartDate) AS [Month], COUNT(MONTH(StartDate)) AS Instances 
-FROM K2ServerLog.ServerLog.ProcInst GROUP BY YEAR(StartDate), MONTH(StartDate) ORDER BY YEAR(StartDate), MONTH(StartDate)
+FROM ServerLog.ProcInst GROUP BY YEAR(StartDate), MONTH(StartDate) ORDER BY YEAR(StartDate), MONTH(StartDate)
