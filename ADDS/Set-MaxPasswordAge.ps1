bcac6b9bd5e82f08846a1be181e6730d3d42e4e3
Mikhail <rodionovmv@gmail.com>
2017-10-17 14:11:36 +0300

    Update Set-MaxPasswordAge.ps1

--- ADDS/Set-MaxPasswordAge.ps1
+++ ADDS/Set-MaxPasswordAge.ps1
@@ -1,2 +1,3 @@
 # Disable passwords expiration
 Set-ADDefaultDomainPasswordPolicy -Identity domain.com -MaxPasswordAge 0
+Set-ADDefaultDomainPasswordPolicy -Identity domain.com -PasswordHistoryCount 0
