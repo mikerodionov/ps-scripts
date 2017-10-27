2a3044709ee5c6692485ccc2a82b538df68bf9a6
Mikhail <rodionovmv@gmail.com>
2017-10-27 13:59:53 +0300

    Update K2-Kerberos-SPNs.ps1

--- K2/K2-Kerberos-SPNs.ps1
+++ K2/K2-Kerberos-SPNs.ps1
@@ -1,18 +1,25 @@
+# In Windows Server 2012, SetSPN will no longer be able to register duplicate SPNs in a domain. 
+# When SetSPN –a is used, SetSPN will treat it as SetSPN –s.
+# Source: https://technet.microsoft.com/en-us/library/hh831747.aspx
+
 #Host Server:
-setspn – A BlackPearlServer/k2:5555 denallix\k2service
-setspn – A BlackPearlServer/k2.denallix.com:5555 denallix\k2service
+setspn –A K2Server/k2:5555 denallix\k2service
+setspn -A K2Server/k2.denallix.com:5555 denallix\k2service
 
 #Workflow Server 
-
-setspn -S K2Server/k2:5252 denallix\k2service 
-setspn -S K2Server/k2.denallix.com:5252 denallix\k2service
+setspn -A K2Server/k2:5252 denallix\k2service 
+setspn -A K2Server/k2.denallix.com:5252 denallix\k2service
 
 #Workspace Application Pool Identity Account
-
-setspn -S HTTP/k2 denallix\k2webservice
-setspn -S HTTP/k2.denallix.com denallix\k2webservice
+setspn -A HTTP/k2 denallix\k2webservice
+setspn -A HTTP/k2.denallix.com denallix\k2webservice
 
 #If the web site uses Host Header, create SPNs as the following:
+setspn -A HTTP/k2 denallix\k2webservice
+setspn -A HTTP/k2.denallix.com denallix\k2webservice
 
-setspn -S HTTP/k2 denallix\k2webservice
-setspn -S HTTP/k2.denallix.com denallix\k2webservice
+# Display registered SPNs:
+setspn -T denallix -Q */K2*
+# Check per specific account:
+setspn -L denallix\k2service
+setspn -L denallix\k2webservice
