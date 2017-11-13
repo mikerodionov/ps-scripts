1b41afc02de797c32e5df745c5cf48bc99e92cfe
Mikhail <rodionovmv@gmail.com>
2017-11-13 14:12:39 +0300

    Create Grant_SPShelladmin_DB_role.ps1

---
+++ SharePoint/Grant_SPShelladmin_DB_role.ps1
@@ -0,0 +1,11 @@
+# K2 for SharePoint App Upload User Account requirements:
+# Local Administrator on the SharePoint Server
+# Site Collection Administrator on the App Catalog Site Collection
+# db_owner rights on the SharePoint_Config Database
+# db_datareader role on the SharePoint_Config Database
+# SharePoint Shell Access role
+# SPShelladmin DB role
+
+# Grant SPShelladmin DB role on App Management DB
+# Use Get-SPDatabase to get App Management DB ID
+Add-SPShellAdmin -UserName Denallix\Administrator -database %database_ID%
