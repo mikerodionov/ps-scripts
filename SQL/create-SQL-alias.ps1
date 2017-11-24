152dd4e491f4707870ddbb158419f223a9fb547d
Mikhail <rodionovmv@gmail.com>
2017-11-24 12:08:59 +0300

    Update create-SQL-alias.ps1

--- SQL/create-SQL-alias.ps1
+++ SQL/create-SQL-alias.ps1
@@ -1,7 +1,9 @@
+# This script takes local machine name for SQL Server name ($ServerName)
+# and creates alias "K2SQL" ($AliasName)
+# Tested on: Windows Server 2016
+
 $ServerName = Get-Content Env:Computername
 $AliasName = "K2SQL" 
-$SN
-$AliasName
  
 $64bit = "HKLM:\SOFTWARE\Microsoft\"
 $32bit = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\"
@@ -9,20 +11,20 @@ $32bit = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\"
 ## Check if the ConnectTo SubFolder exists in the registry if not create it
 If((Test-Path -Path ($64bit + "MSSQLServer\Client\ConnectTo"))-ne $true)
     {
-    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("ConnectTo")
+    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Microsoft\MSSQLServer\Client", $true).CreateSubKey("ConnectTo")
     }
 If((Test-Path -Path ($32bit + "MSSQLServer\Client\ConnectTo"))-ne $true)
     {
-    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("ConnectTo")
+    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client", $true).CreateSubKey("ConnectTo")
     }
 
 If((Test-Path -Path ($64bit + "MSSQLServer\Client\SuperSocketNetLib"))-ne $true)
     {
-    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("SuperSocketNetLib")
+    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Microsoft\MSSQLServer\Client", $true).CreateSubKey("SuperSocketNetLib")
     }
 If((Test-Path -Path ($32bit + "MSSQLServer\Client\SuperSocketNetLib"))-ne $true)
     {
-    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client", $writable).CreateSubKey("SuperSocketNetLib")
+    $key = (get-item HKLM:\).OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client", $true).CreateSubKey("SuperSocketNetLib")
     }
  
 ## Create the TCPAlias
