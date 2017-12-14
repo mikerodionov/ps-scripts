75d7d633b5817f65fc4b04af85e9056d7ff8b3c1
Mikhail <rodionovmv@gmail.com>
2017-12-14 17:25:20 +0300

    Update disable_server_manager.ps1

--- Windows_Server_Generic/disable_server_manager.ps1
+++ Windows_Server_Generic/disable_server_manager.ps1
@@ -1,2 +1,3 @@
 # This script disables automatic launch of Server Manager
-New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" -Force
+# If you want to set it for current user only change -Path to HKCU
+New-ItemProperty -Path HKLM:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" -Force
