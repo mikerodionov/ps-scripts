3d80c9b1a88c64ebc971100f7ab328b30f554604
Mikhail <rodionovmv@gmail.com>
2018-01-23 12:14:47 +0300

    Create set-ost-location.ps1

---
+++ ps-registry/set-ost-location.ps1
@@ -0,0 +1,5 @@
+$Outlook2016='HKCU\Software\Microsoft\Office\16.0\Outlook\'
+$Outlook2013='HKCU\Software\Microsoft\Office\15.0\Outlook\'
+$Outlook2010='HKCU\Software\Microsoft\Office\14.0\Outlook\'
+
+Test-Path $Outlook2016
