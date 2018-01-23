21ccb873638bc8cb3082a0b78d4dac4916945cb8
Mikhail <rodionovmv@gmail.com>
2018-01-23 12:45:45 +0300

    Update set-ost-location.ps1

--- ps-registry/set-ost-location.ps1
+++ ps-registry/set-ost-location.ps1
@@ -1,5 +1,7 @@
-$Outlook2016='HKCU\Software\Microsoft\Office\16.0\Outlook\'
-$Outlook2013='HKCU\Software\Microsoft\Office\15.0\Outlook\'
-$Outlook2010='HKCU\Software\Microsoft\Office\14.0\Outlook\'
+$Outlook2016='HKCU:\Software\Microsoft\Office\16.0\Outlook\'
+$Outlook2013='HKCU:\Software\Microsoft\Office\15.0\Outlook\'
+$Outlook2010='HKCU:\Software\Microsoft\Office\14.0\Outlook\'
 
 Test-Path $Outlook2016
+
+New-ItemProperty $Outlook2016 -Name ForcePstPath -Value "C:\Outlook"
