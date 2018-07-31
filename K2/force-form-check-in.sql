1d5368b4162944bb4c286e6bf6123fdd45a348c9
Mikhail <rodionovmv@gmail.com>
2018-07-31 11:30:53 +0300

    Update force-form-check-in.sql

--- K2/force-form-check-in.sql
+++ K2/force-form-check-in.sql
@@ -1,6 +1,8 @@
 --Be sure to back up your K2 database before use this script
 --Limitation: Only works if form has been checked out, does not work for newly created form which has not been explicitly checked out
 --I.e. script will fail if form was created and not checked in
+--Tested with: K2 4.6.11
+--Adjust K2 database name as necessary on the line below (in case you are using non-default K2 database name)
 USE K2
 DECLARE @FORM_NAME AS varchar(128)
 DECLARE @FORM_ID AS nvarchar(max)
