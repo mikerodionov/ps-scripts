8cca4032230831ab5c27705f8cff024778c71368
Mikhail <rodionovmv@gmail.com>
2018-07-31 11:24:23 +0300

    Update force-form-check-in.sql

--- K2/force-form-check-in.sql
+++ K2/force-form-check-in.sql
@@ -1,12 +1,23 @@
+--Be sure to back up your K2 database before use this script
+--Limitation: Only works if form has been checked out, does not work for newly created form which has not been explicitly checked out
+--I.e. script will fail if form was created and not checked in
 USE K2
-SELECT * FROM Form.Form_Design WHERE Name='%FORM_NAME%'
--- Copy ID and CheckedOutBy values from the output to reuse as a parameters below
-
-DECLARE @myvar AS uniqueidentifier
---Replace value below with value of ID
---That conversion required sometimes
-set @myvar = '125083A3-1090-4AAB-BAC3-920A6446AC26'
---Replace @UserID value with one from CheckedOutBy or another:
-EXEC Form.aUndoFormsCheckOut @UserID = 'K2:DOMAIN\user', @IDs = @myvar;
--- Replace 'UserName' with actual username
--- Replace ID with actual Form ID
+DECLARE @FORM_NAME AS varchar(128)
+DECLARE @FORM_ID AS nvarchar(max)
+DECLARE @USER AS nvarchar(200)
+--Make sure to specify valid form name
+--Script will perform check in on behalf of the user who performed checked out
+--If necessary you can explicitly specify different user
+SET @FORM_NAME = 'Your form name goes here'
+SET @USER = (SELECT CheckedOutBy FROM Form.Form_Design WHERE Name=@FORM_NAME) 
+IF EXISTS (SELECT ID FROM Form.Form_Design WHERE Name=@FORM_NAME) 
+                 AND EXISTS (SELECT Name FROM [Identity].[Identity] WHERE FQN=@USER)
+BEGIN
+    PRINT 'Specified form name: ' + @FORM_NAME;
+	PRINT 'CheckedOutBy value: ' + @USER;
+	SET @FORM_ID = (SELECT ID FROM Form.Form_Design WHERE Name=@FORM_NAME)
+	EXEC Form.aUndoFormsCheckOut @UserID = @USER, @IDs = @FORM_ID;
+	PRINT 'Form ' + @FORM_NAME + ' has been checked out on behalf of ' + @USER;
+END
+ELSE
+	PRINT 'Form: "' + @FORM_NAME + '" does not exist or is not checked out.'
