79e8d5ec6057e3537bd1f0cfecb75f0b4ab8870b
Mikhail <rodionovmv@gmail.com>
2018-08-14 16:32:59 +0300

    Update SQL_DB_index_fragmentation_check.sql

--- SQL/SQL_DB_index_fragmentation_check.sql
+++ SQL/SQL_DB_index_fragmentation_check.sql
@@ -26,3 +26,16 @@ ON ind.object_id = indexstats.object_id
 AND ind.index_id = indexstats.index_id 
 WHERE indexstats.avg_fragmentation_in_percent > 30 AND indexstats.page_count > 5000
 ORDER BY indexstats.avg_fragmentation_in_percent DESC
+
+/*
+--Sample index rebuild scripts generator
+SELECT 
+('ALTER INDEX ' + ind.name + ' ON ' + SCHEMA_NAME(schema_id) + '.' + OBJECT_NAME(ind.OBJECT_ID) + ' REBUILD  WITH (ONLINE = ON)') 
+FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) indexstats 
+INNER JOIN sys.indexes ind  
+ON ind.object_id = indexstats.object_id 
+AND ind.index_id = indexstats.index_id 
+INNER JOIN sys.tables sch
+ON sch.object_id = indexstats.object_id 
+WHERE indexstats.avg_fragmentation_in_percent > 30 AND indexstats.page_count > 5000
+*/
