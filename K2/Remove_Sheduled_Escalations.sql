0db29a7281ed1ab8d864bde4ededfa98a0dd8a9c
Mikhail <rodionovmv@gmail.com>
2018-10-28 15:29:17 +0100

    Create Remove_Sheduled_Escalations.sql

---
+++ K2/Remove_Sheduled_Escalations.sql
@@ -0,0 +1,5 @@
+DECLARE @VersionWhereEscalationWasDeleted INT;
+DECLARE @ProcessWhichWillExpire NVARCHAR(MAX);
+SET @VersionWhereEscalationWasDeleted = 7; -- (SAMPLE)
+SET @ProcessWhichWillExpire = N'Full\Name\Of\Process'; -- (SAMPLE) 'HR\EmployeeOnBoarding'
+DELETE [AS] FROM [Server].[Async] AS [AS] WHERE [AS].[Type] = 5 AND [AS].[ProcInstID] IN ( SELECT [PI].[ID] FROM [Server].[ProcInst] AS [PI] INNER JOIN [Server].[Proc] AS [EP] ON [PI].[ExecutingProcID] = [EP].[ID] -- Executing INNER JOIN [Server].[Proc] AS [OP] ON [PI].[ProcID] = [OP].[ID] -- Original INNER JOIN [Server].[ProcSet] AS [EPS] ON [EP].[ProcSetID] = [EPS].[ID] INNER JOIN [Server].[ProcSet] AS [OPS] ON [OP].[ProcSetID] = [OPS].[ID] WHERE [OPS].[FullName] = @ProcessWhichWillExpire AND [OP].[Ver] < @VersionWhereEscalationWasDeleted AND [EP].[Ver] >= @VersionWhereEscalationWasDeleted );
