773b80e3dba2d12dc4f2191b00b589645911a0c6
Mikhail <rodionovmv@gmail.com>
2018-10-30 14:41:01 +0100

    Update sql-view-K2-environment-fields.sql

--- K2/sql-view-K2-environment-fields.sql
+++ K2/sql-view-K2-environment-fields.sql
@@ -2,3 +2,7 @@ SELECT [Field].[FieldId], [Field].[FieldName], [FieldValue].[FieldValue]
   FROM [Environment].[Field]
   INNER JOIN [Environment].[FieldValue] ON [Field].[FieldId] = [FieldValue].[FieldId]
   ORDER BY FieldName
+
+--Find and replace example
+UPDATE [Environment].[FieldValue]
+SET FieldValue = REPLACE(FieldValue, 'FIND WHAT', 'REPLACE WITH')
