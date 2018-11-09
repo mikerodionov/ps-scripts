aa8847cfae81105ae4b8a0ed11d66e1d347f6e1a
Mikhail <rodionovmv@gmail.com>
2018-11-09 16:13:16 +0100

    Update service-restarts-stat.ps1

--- K2/service-restarts-stat.ps1
+++ K2/service-restarts-stat.ps1
@@ -2,4 +2,4 @@
 # Run on K2 server which hosts K2 service
 Get-EventLog -LogName system -Source "service control manager" |?{$_.message -match "blackpearl"} | select timegenerated, message -First 30 | Export-Csv "C:\Servie_Restarts_$env:computername.xls"
 # Option which will only show "terminated unexpectedly" event
-Get-EventLog -LogName system -Source "service control manager" |?{$_.message -match "blackpearl"} | ?{$_.message -match "unexpectedly"} | select timegenerated, message -First 30
+Get-EventLog -LogName system -Source "service control manager" |?{$_.message -match "blackpearl"} | ?{$_.message -match "unexpectedly"} | select timegenerated, message -First 30 | Export-Csv "C:\Servie_Restarts_$env:computername.xls"
