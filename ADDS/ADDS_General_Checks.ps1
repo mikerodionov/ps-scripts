b52c188dd2a083e9b7acb18735bb1f7691d52794
Mikhail <rodionovmv@gmail.com>
2018-11-16 23:13:46 +0100

    Update ADDS_General_Checks.ps1

--- ADDS/ADDS_General_Checks.ps1
+++ ADDS/ADDS_General_Checks.ps1
@@ -1,4 +1,9 @@
-#Specifiy name of machine to which you need to obtain OU
+# ADDS Domain quick check
+# v.1.0
+# Mikhail Rodionov - mikerodionov.com
+
+Function ADDSCHK ()
+{
 $pc=hostname
 $PC_OU=(Get-ADComputer $pc).DistinguishedName
 #Get total number of users (enabled and disabled)
@@ -10,8 +15,9 @@ $TNDU=(Get-ADUser -filter * |Where {$_.enabled -ne "False"}).count
 #Get number of groups
 $TNG=(Get-ADGroup -filter *).count
 
-$pc organizational unit: $PC_OU
-Total number of domain users is $TNU
-Total number of enabled domain users is $TNEU
-Total number of disabled domain users is $TNDU
-Total number of domaim groups is $TNG
+write-host 'Current server organizational unit:' $PC_OU -ForegroundColor Green
+write-host 'Total number of domain users is ' $TNU -ForegroundColor Green
+write-host 'Total number of enabled domain users is ' $TNEU -ForegroundColor Green
+write-host 'Total number of disabled domain users is ' $TNDU -ForegroundColor Green
+write-host 'Total number of domaim groups is ' $TNG -ForegroundColor Green
+}
