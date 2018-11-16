41df399d5dcf3b49730e415f13e06f9493b0a39d
Mikhail <rodionovmv@gmail.com>
2018-11-16 22:41:22 +0100

    Create ADDS_General_Checks.ps1

---
+++ ADDS/ADDS_General_Checks.ps1
@@ -0,0 +1,11 @@
+#Specifiy name of machine to which you need to obtain OU
+$pc='dlx'
+(Get-ADComputer $pc).DistinguishedName
+
+#Get total number of users (enabled and disabled)
+
+#Get total number of enabled users
+
+#Get total number of disabled users
+
+#Get number of groups
