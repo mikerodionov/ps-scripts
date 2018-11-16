af8441e4aec94f997db87c1f765fdd062e09b6a5
Mikhail <rodionovmv@gmail.com>
2018-11-16 22:43:48 +0100

    Update ADDS_General_Checks.ps1

--- ADDS/ADDS_General_Checks.ps1
+++ ADDS/ADDS_General_Checks.ps1
@@ -3,9 +3,10 @@ $pc='dlx'
 (Get-ADComputer $pc).DistinguishedName
 
 #Get total number of users (enabled and disabled)
-
+(Get-ADUser -filter *).count
 #Get total number of enabled users
-
+(Get-AdUser -filter * |Where {$_.enabled -eq "True"}).count
 #Get total number of disabled users
-
+(Get-ADUser -filter * |Where {$_.enabled -ne "False"}).count
 #Get number of groups
+(Get-ADGroup -filter *).count
