dbc6205f6d8acb8556086558b34e815bcbec04c7
Mikhail <rodionovmv@gmail.com>
2019-03-11 16:35:57 +0100

    Update enable_fusion_logging.ps1

--- ps-registry/enable_fusion_logging.ps1
+++ ps-registry/enable_fusion_logging.ps1
@@ -1,9 +1,53 @@
-$RegKey = “HKLM:\SOFTWARE\Microsoft\Fusion”
-if (-Not(Test-Path “$RegKey”)) {
-    New-Item -Path $RegKey -Force
+$path = "C:\FusionLog"
+If(!(test-path $path))
+{
+   New-Item -ItemType Directory -Force -Path $path
 }
-New-ItemProperty -Path $RegKey -Name “ForceLog” -PropertyType  Dword -Value 1
+
+$RegKey = “HKLM:\SOFTWARE\Microsoft\Fusion”
+
+If(!((Get-Item -Path $RegKey).GetValue("ForceLog") -ne $nul))
+{
+New-ItemProperty -Path $RegKey -Name “ForceLog” -PropertyType Dword -Value 1
+}
+Else
+{
+Set-ItemProperty -Path $RegKey -Name “ForceLog” -Value 1 -Force
+}
+
+If(!((Get-Item -Path $RegKey).GetValue("LogFailures") -ne $nul))
+{
 New-ItemProperty -Path $RegKey -Name “LogFailures” -PropertyType Dword -Value 1
+}
+Else
+{
+Set-ItemProperty -Path $RegKey -Name “LogFailures” -Value 1 -Force
+}
+
+If(!((Get-Item -Path $RegKey).GetValue("LogResourceBinds") -ne $nul))
+{
 New-ItemProperty -Path $RegKey -Name “LogResourceBinds” -PropertyType Dword -Value 1
+}
+Else
+{
+Set-ItemProperty -Path $RegKey -Name “LogResourceBinds” -Value 1 -Force
+}
+
+
+If(!((Get-Item -Path $RegKey).GetValue("EnableLog") -ne $nul))
+{
 New-ItemProperty -Path $RegKey -Name “EnableLog” -PropertyType Dword -Value 1
+}
+Else
+{
+Set-ItemProperty -Path $RegKey -Name “EnableLog” -Value 1 -Force
+}
+
+If(!((Get-Item -Path $RegKey).GetValue("LogPath") -ne $nul))
+{
 New-ItemProperty -Path $RegKey -Name “LogPath” -PropertyType String -Value "C:\FusionLog\"
+}
+Else
+{
+Set-ItemProperty -Path $RegKey -Name “LogPath” -Value "C:\FusionLog\"
+}
