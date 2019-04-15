9307290b2d61a5dc6df70c141c20865cf1efdc62
Mikhail <rodionovmv@gmail.com>
2019-04-15 14:05:32 +0200

    Update check_installed_SQL_Server_version.ps1

--- SQL/check_installed_SQL_Server_version.ps1
+++ SQL/check_installed_SQL_Server_version.ps1
@@ -1,6 +1,8 @@
+# This scripts detecs installed SQL instances through registry and returns version information
 $inst = (get-itemproperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances
 foreach ($i in $inst)
 {
+$p=(Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL').$i
 (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL').$i
 (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$p\Setup").Edition
 (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$p\Setup").Version
@@ -13,3 +15,11 @@ $ver = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$p\Setup
         default {"Unknown Version"; break}
     }
 }
+# Part below performs switch to Setup Bootstrap directory of SQL Server installation detected
+switch ($ver.Substring(0,2)) {
+    "14" {cd "C:\Program Files\Microsoft SQL Server\140\Setup Bootstrap\SQL2017"; break} 
+    "13" {cd "C:\Program Files\Microsoft SQL Server\130\Setup Bootstrap\SQLServer2016"; break}
+    "12" {cd "C:\Program Files\Microsoft SQL Server\120\Setup Bootstrap\SQLServer2014"; break}
+    "11" {cd "C:\Program Files\Microsoft SQL Server\110\Setup Bootstrap\SQLServer2012"; break}
+    default {"Unknown Version"; break}
+    }
