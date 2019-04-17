529fee06854e72881f842124cf9355a00fd1db4d
Mikhail <rodionovmv@gmail.com>
2019-04-17 16:15:20 +0200

    Update Get_SmOs_for_SI.sql

--- K2/Get_SmOs_for_SI.sql
+++ K2/Get_SmOs_for_SI.sql
@@ -1,6 +1,6 @@
 -- This script returns list of all SmOs based on SI with specific GUID
-SELECT DISTINCT SystemName as SMOSystemName, DisplayName_XML as SMODisplayName, SmartObjectGUID, SO.ID.value('@guid', 'nvarchar(100)') as ServiceInstanceGUID
+SELECT DISTINCT SystemName as SMOSystemName, DisplayName_XML AS SMODisplayName, SmartObjectGUID, SO.ID.value('@guid', 'nvarchar(100)') AS ServiceInstanceGUID
 FROM [SmartBroker].[SmartObject]
-CROSS APPLY SmartObjectXML.nodes('/smartobjectroot/methods/method/serviceinstances/serviceinstance[1]') as SO(ID)
-WHERE SO.ID.value('@guid', 'nvarchar(100)') = '%INSERT_SI_GUID%' --Replace with Service Instance GUID
+CROSS APPLY SmartObjectXML.nodes('/smartobjectroot/methods/method/serviceinstances/serviceinstance[1]') AS SO(ID)
+WHERE SO.ID.value('@guid', 'nvarchar(100)') = '%INSERT_SI_GUID%'
 ORDER BY SMOSystemName
