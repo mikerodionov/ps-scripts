3e8fa890d33b7bae36dc5a1570e5d134f10c702f
Mikhail <rodionovmv@gmail.com>
2019-05-15 13:53:00 +0200

    Update bak_file_SQL_version_check.ps1

--- SQL/bak_file_SQL_version_check.ps1
+++ SQL/bak_file_SQL_version_check.ps1
@@ -1,6 +1,6 @@
 #This script retrieves SQL Server version from BAK file and allows you to compare with your SQL Server version
 #If your SQL Server version is lower that one which was  used to create backup you won't be able to restore BAK file
-$BakFile='C:\backup.bak'
+$BakFile= Read-Host "Provide full path to SQL BAK file (including file name with extension)"
 $VerDetectQuery="RESTORE HEADERONLY FROM DISK='$BakFile'"
 $BakMajorVer=Invoke-Sqlcmd -Query $VerDetectQuery | Select-Object -ExpandProperty SoftwareVersionMajor
 $BakMinorVer=Invoke-Sqlcmd -Query $VerDetectQuery | Select-Object -ExpandProperty SoftwareVersionMinor
@@ -11,3 +11,11 @@ $ServerSQLVer=Invoke-Sqlcmd -Query "SELECT SERVERPROPERTY('productversion')" | S
 
 Write-Host "Backup file SQL Server version is: " $BakVer
 Write-Host "Your SQL Server version is:        " $ServerSQLVer
+If ($BakVer -le $ServerSQLVer)
+    {
+    Write-Host "You can restore this backup on your server" -BackgroundColor Green
+    }
+    Else
+    {
+    Write-Host "You cannot restore backup on your server, to be able to  do that you need to upgrade SQL Server version" -BackgroundColor Red
+    }
