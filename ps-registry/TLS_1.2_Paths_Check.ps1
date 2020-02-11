ef39a93fb876492fece51cb857beabe6312ebdc6
Mikhail <rodionovmv@gmail.com>
2020-02-11 18:49:19 +0100

    Update TLS_1.2_Paths_Check.ps1

--- ps-registry/TLS_1.2_Paths_Check.ps1
+++ ps-registry/TLS_1.2_Paths_Check.ps1
@@ -1,21 +1,7 @@
 # TLS 1.2 Minimum Requirement Verify
-#1
 Test-Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727'
-#2
 Test-Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v2.0.50727'
-#3
 Test-Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319'
-#4
-Test-Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319'
-#5
 Test-Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319'
-#6
-Test-Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319'
-#7
 Test-Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client'
-#8
-Test-Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client'
-#9
-Test-Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server'
-#10
 Test-Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server'
