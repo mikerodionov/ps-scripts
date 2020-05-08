cf25dd63948674eeaf4a68ec47a40d9abdfad0bb
Mikhail <rodionovmv@gmail.com>
2020-05-08 09:36:15 +0200

    Update service-restarts-stat.ps1

--- K2/service-restarts-stat.ps1
+++ K2/service-restarts-stat.ps1
@@ -1,4 +1,4 @@
-# This script will grab 30 latest service restart event I write output into C:\%servername%.xls
+# This script will grab 30 latest service restart event and write output into C:\%servername%.xls
 # Run on K2 server which hosts K2 service
 Get-EventLog -LogName system -Source "service control manager" |?{$_.message -match "blackpearl"} | select timegenerated, message -First 30 | Export-Csv "C:\Servie_Restarts_$env:computername.xls"
 # Option which will only show "terminated unexpectedly" event
