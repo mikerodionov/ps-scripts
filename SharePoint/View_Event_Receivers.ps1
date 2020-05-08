1c6f858a57d80044d2d81e3ae0349eb7f9dbcb9f
Mikhail <rodionovmv@gmail.com>
2020-05-08 16:14:53 +0200

    Update View_Event_Receivers.ps1

--- SharePoint/View_Event_Receivers.ps1
+++ SharePoint/View_Event_Receivers.ps1
@@ -1,11 +1,11 @@
-# Prompts the user for parameters;
+# Prompt the user for parameters
 $webURL = Read-Host “URL of the Web”
 $listName = Read-Host “Name of the list/library”
 
-# Get a reference to the SharePoint web;
+# Get a reference to the SharePoint web
 $web = Get-SPWeb $webURL
 
-# Get a reference to the SharePoint list or library;
+# Get a reference to the SharePoint list or library
 $list = $web.Lists[$listName]
 foreach($eventReceiverDef in $list.EventReceivers)
 {
