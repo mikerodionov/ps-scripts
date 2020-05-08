4d048826acd253c886d3e8b153d3bd271337a24f
Mikhail <rodionovmv@gmail.com>
2020-05-08 16:13:47 +0200

    Create View_Event_Receivers.ps1

---
+++ SharePoint/View_Event_Receivers.ps1
@@ -0,0 +1,18 @@
+# Prompts the user for parameters;
+$webURL = Read-Host “URL of the Web”
+$listName = Read-Host “Name of the list/library”
+
+# Get a reference to the SharePoint web;
+$web = Get-SPWeb $webURL
+
+# Get a reference to the SharePoint list or library;
+$list = $web.Lists[$listName]
+foreach($eventReceiverDef in $list.EventReceivers)
+
+{
+    if($eventReceiverDef.Name -ne “”)
+    {
+                    $eventInfo = $eventReceiverDef.Name + ” – ” + $eventReceiverDef.Type
+                    Write-Host $eventInfo -BackgroundColor Green -ForegroundColor Black
+    }
+}
