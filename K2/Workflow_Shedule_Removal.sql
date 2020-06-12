c1a8cf7b7514dd2e1a646e40bf9c8f7b7631214f
Mikhail <rodionovmv@gmail.com>
2020-06-12 09:49:02 +0200

    Update Workflow_Shedule_Removal.sql

--- K2/Workflow_Shedule_Removal.sql
+++ K2/Workflow_Shedule_Removal.sql
@@ -7,9 +7,9 @@ SET @NAME = 'Test 1'
 SET @ID = (SELECT ID FROM [K2].[Eventbus].[Schedule] WHERE Name =  @NAME)
 SET @CUSTOMEVENTID = (SELECT ID FROM [K2].[Eventbus].[CustomEvent] WHERE Name = @NAME)
 
-DELETE FROM [K2].[Eventbus].[EventPolicyMappingInstance] WHERE ID=@CUSTOMEVENTID
-DELETE FROM [K2].[Eventbus].[EventPolicyMapping] WHERE CustomEventID=@CUSTOMEVENTID
-DELETE FROM [K2].[Eventbus].[CustomEvent] WHERE ID=@CUSTOMEVENTID
-DELETE FROM [K2].[Eventbus].[CustomEventInstance] WHERE ID=@CUSTOMEVENTID
+DELETE FROM [K2].[Eventbus].[EventPolicyMappingInstance] WHERE ID = @CUSTOMEVENTID
+DELETE FROM [K2].[Eventbus].[EventPolicyMapping] WHERE CustomEventID = @CUSTOMEVENTID
+DELETE FROM [K2].[Eventbus].[CustomEvent] WHERE ID = @CUSTOMEVENTID
+DELETE FROM [K2].[Eventbus].[CustomEventInstance] WHERE ID = @CUSTOMEVENTID
 DELETE FROM [K2].[Eventbus].[ScheduleInstance] WHERE ID = @ID
 DELETE FROM [K2].[Eventbus].[Schedule] WHERE ID = @ID
