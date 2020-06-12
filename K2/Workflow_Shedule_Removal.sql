0b21b190394b6826baa2666bd621f028787512d7
Mikhail <rodionovmv@gmail.com>
2020-06-12 09:48:30 +0200

    Update Workflow_Shedule_Removal.sql

--- K2/Workflow_Shedule_Removal.sql
+++ K2/Workflow_Shedule_Removal.sql
@@ -1,10 +1,13 @@
+-- Backup your K2 DB before run
 DECLARE @NAME VARCHAR(256);
 DECLARE @ID INT;
 DECLARE @CUSTOMEVENTID INT;
+-- Specify schedule name below
 SET @NAME = 'Test 1'
 SET @ID = (SELECT ID FROM [K2].[Eventbus].[Schedule] WHERE Name =  @NAME)
 SET @CUSTOMEVENTID = (SELECT ID FROM [K2].[Eventbus].[CustomEvent] WHERE Name = @NAME)
 
+DELETE FROM [K2].[Eventbus].[EventPolicyMappingInstance] WHERE ID=@CUSTOMEVENTID
 DELETE FROM [K2].[Eventbus].[EventPolicyMapping] WHERE CustomEventID=@CUSTOMEVENTID
 DELETE FROM [K2].[Eventbus].[CustomEvent] WHERE ID=@CUSTOMEVENTID
 DELETE FROM [K2].[Eventbus].[CustomEventInstance] WHERE ID=@CUSTOMEVENTID
