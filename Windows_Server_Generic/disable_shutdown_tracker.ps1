3a0b5580b0161f14cc281482fe28210442132a41
Mikhail <rodionovmv@gmail.com>
2020-10-02 17:30:26 +0200

    Update disable_shutdown_tracker.ps1

--- Windows_Server_Generic/disable_shutdown_tracker.ps1
+++ Windows_Server_Generic/disable_shutdown_tracker.ps1
@@ -1,5 +1,5 @@
 # This script disables Windows Shutdown Event Tracker
-# Works/tested in Windows 2008 - 2016
+# Works/tested in Windows 2008 - 2019
 if ( -Not (Test-Path 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability'))
 {
 New-Item -Path 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT' -Name Reliability -Force
