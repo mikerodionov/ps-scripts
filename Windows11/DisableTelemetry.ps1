c61ba411842549f9db4d0b06424a81a89ec14f7c
Mikhail Rodionov <13007691+mikerodionov@users.noreply.github.com>
2022-10-14 09:14:09 +0200

    Create DisableTelemetry.ps1

---
+++ Windows11/DisableTelemetry.ps1
@@ -0,0 +1,16 @@
+# DataCollection GPO setting available in Windows 10, Windows 11
+# 0 - Disabled
+# 1 - Basic (Required)
+# 2 - Enhanced
+# 3 - Full (Optional) diagnostic data. Desktop Analytics requires at least basic diagnostic data.
+# Microsoft recommends that you use the Optional (limited) (Enhanced (Limited)) level with Desktop Analytics
+$RegKey = “HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection”
+
+If(!((Get-Item -Path $RegKey).GetValue("AllowTelemetry") -ne $nul))
+{
+New-ItemProperty -Path $RegKey -Name “AllowTelemetry” -PropertyType Dword -Value 0
+}
+Else
+{
+Set-ItemProperty -Path $RegKey -Name “AllowTelemetry” -Value 0 -Force
+}
